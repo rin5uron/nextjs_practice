3fcecf3dce8003afa1655ca0b1a7df9488e054a9
r <rin5uron@gmail.com>
2025-08-12 22:41:33 +0900

    コンポーネントについて

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,17 @@
-import Link from 'next/link'; // ページ間を移動するための Next.js のコンポーネント
+import Link from 'next/link';
 
 export default function Header() {
   return (
+    // headerに枠線をつけます
     <header style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
+      {/* navの中身を横並びにします */}
       <nav style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <div>
           <Link href="/" style={{ fontWeight: 'bold', textDecoration: 'none' }}>
             My Next.js Site
           </Link>
         </div>
+        {/* ulの中身を横並びにします */}
         <ul style={{ listStyle: 'none', display: 'flex', gap: '1rem' }}>
           <li><Link href="/">ホーム</Link></li>
           <li><Link href="/news">お知らせ</Link></li>
@@ -19,4 +22,3 @@ export default function Header() {
     </header>
   );
 }
-
